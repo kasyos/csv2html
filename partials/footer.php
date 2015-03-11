d5c719730b8260490737d0f6d0a0cd999ce22f8f
unknown <florian_laroche@hotmail.fr>
2015-03-11 18:51:50 +0100

    Maj footer

--- partials/footer.php
+++ partials/footer.php
@@ -3,7 +3,7 @@
           <div class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <p class="navbar-text">&copy; Copyright <?php echo date('Y'); ?> - <a href="http://kasyos.fr" target="_blank">kasyos.fr</a> - <img src="img/slimyland.png"/> <a href="#">SlimyLand Universe</a></p>
+            <p class="navbar-text">&copy; Copyright <?php echo date('Y'); ?> - <a href="http://kasyos.fr" target="_blank">kasyos.fr</a> - <img src="img/slimyland.png"/> <a href="#">SlimyLand Universe</a> - <img src="img/github.png" width="20px"/> <a href="https://github.com/kasyos/csv2html" target="_blank">Sources disponible sur github !</a> Adhérent d'un web libre !</p>
           </ul>
         </div>
               </div>
